3837351316a9ad5f54ff80df26e83403a9506a22
王六锋 <2496436621@qq.com>
2020-04-28 17:45:17 +0800

    二级导航组件

--- src/components/SecondMenu/ceshi.js
+++ src/components/SecondMenu/ceshi.js
@@ -1,60 +1,63 @@
-const menu = [
-  {
-    title: "员工花名册",
-    path: "/personnel/roster",
-  },
-  {
-    title: "员工关系",
-    path: "",
-    children: [
-      {
-        title: "入职管理",
-        path: "/personnel/entry",
-      },
-      {
-        title: "转正管理",
-        path: "/personnel/formal",
-      },
-      {
-        title: "调动管理",
-        path: "/personnel/transfer",
-      },
-      {
-        title: "离职管理",
-        path: "/personnel/quit",
-      },
-      {
-        title: "合同管理",
-        path: "/personnel/contract",
-      },
-      {
-        title: "员工关怀",
-        path: "/personnel/staff",
-      },
-      {
-        title: "人事异动记录",
-        path: "/personnel/record",
-      },
-    ],
-  },
-  {
-    title: "统计分析",
-    path: "",
-    children: [
-      {
-        title: "员工数量统计",
-        path: "/personnel/number",
-      },
-      {
-        title: "在职员工统计",
-        path: "/personnel/incumbency",
-      },
-      {
-        title: "离职员工统计",
-        path: "/personnel/statistics",
-      },
-    ],
-  },
-];
-
-export { menu };
+const sidebarObject = {
+  openKeys: ["员工关系", "统计分析"],
+  name: "智能人事",
+  menu: [
+    {
+      title: "员工花名册",
+      path: "/personnel/roster",
+    },
+    {
+      title: "员工关系",
+      path: "",
+      children: [
+        {
+          title: "入职管理",
+          path: "/personnel/entry",
+        },
+        {
+          title: "转正管理",
+          path: "/personnel/formal",
+        },
+        {
+          title: "调动管理",
+          path: "/personnel/transfer",
+        },
+        {
+          title: "离职管理",
+          path: "/personnel/quit",
+        },
+        {
+          title: "合同管理",
+          path: "/personnel/contract",
+        },
+        {
+          title: "员工关怀",
+          path: "/personnel/staff",
+        },
+        {
+          title: "人事异动记录",
+          path: "/personnel/record",
+        },
+      ],
+    },
+    {
+      title: "统计分析",
+      path: "",
+      children: [
+        {
+          title: "员工数量统计",
+          path: "/personnel/number",
+        },
+        {
+          title: "在职员工统计",
+          path: "/personnel/incumbency",
+        },
+        {
+          title: "离职员工统计",
+          path: "/personnel/statistics",
+        },
+      ],
+    },
+  ],
+};
+export { sidebarObject };
--- src/components/SecondMenu/index.jsx
+++ src/components/SecondMenu/index.jsx
@@ -1,11 +1,46 @@
 import React from "react";
 import { classPrefix } from "../../const";
 import "./index.less";
+import { sidebarObject } from "./ceshi";
+import { Menu } from "antd";
+const { SubMenu } = Menu;
+
 class SecondMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
+    const { openKeys, menu } = sidebarObject;
     return (
       <div className={`${classPrefix}-component-secondmenu`}>
-        <div className={`${classPrefix}-component-secondmenu-content`}></div>
+        <div className={`${classPrefix}-component-secondmenu-content`}>
+          <Menu
+            mode="inline"
+            openKeys={openKeys}
+            onOpenChange={(e) => this.setState({ openKeys: e })}
+            className="menu1"
+          >
+            {menu.map((item) => {
+              return item.path ? (
+                <Menu.Item key={item.title} title={item.title}>
+                  {item.title}
+                </Menu.Item>
+              ) : (
+                <SubMenu key={item.title} title={<span>{item.title}</span>}>
+                  {item.children &&
+                    item.children.length &&
+                    item.children.map((it) => (
+                      <Menu.Item key={it.title} title={it.title}>
+                        {it.title}
+                      </Menu.Item>
+                    ))}
+                </SubMenu>
+              );
+            })}
+          </Menu>
+        </div>
       </div>
     );
   }
